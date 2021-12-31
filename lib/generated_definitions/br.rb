e1af35c46a19108095365a02657df0fea81cdd24
Steven Chern <stevenchern@gmail.com>
2021-12-31 03:48:36 +0000

    a

--- lib/generated_definitions/br.rb
+++ lib/generated_definitions/br.rb
@@ -12,19 +12,16 @@ module Holidays
 
     def self.holidays_by_month
       {
-      0 => [{:function => "easter(year)", :function_arguments => [:year], :function_modifier => -47, :name => "Carnaval", :regions => [:br]},
+                0 => [{:function => "easter(year)", :function_arguments => [:year], :function_modifier => -47, :type => :informal, :name => "Carnaval", :regions => [:br]},
             {:function => "easter(year)", :function_arguments => [:year], :function_modifier => -2, :name => "Sexta-feira Santa", :regions => [:br]},
+            {:function => "easter(year)", :function_arguments => [:year], :name => "Páscoa", :regions => [:br]},
             {:function => "easter(year)", :function_arguments => [:year], :function_modifier => 60, :name => "Corpus Christi", :regions => [:br]}],
-      1 => [{:mday => 1, :name => "Dia da Confraternização Universal", :regions => [:br]},
-            {:mday => 25}, {:name => 'Aniversário de São Paulo'}, {:regions => [:br]}],
+      1 => [{:mday => 1, :name => "Dia da Confraternização Universal", :regions => [:br]}],
       4 => [{:mday => 21, :name => "Dia de Tiradentes", :regions => [:br]}],
       5 => [{:mday => 1, :name => "Dia do Trabalho", :regions => [:br]}],
-      6 => [{:mday => 9, :name => "Revolução Constitucionalista", :regions => [:br]}],
       9 => [{:mday => 7, :name => "Proclamação da Independência", :regions => [:br]}],
-      10 => [{:mday => 12, :name => "Dia de Nossa Senhora Aparecida", :regions => [:br]},
-            {:mday => 28, :name => "Dia do servidor público", :regions => [:br]}],
+      10 => [{:mday => 12, :name => "Dia de Nossa Senhora Aparecida", :regions => [:br]}],
       11 => [{:mday => 2, :name => "Dia de Finados", :regions => [:br]},
-            {:mday => 20, :name => "Conciência Negra", :regions => [:br]},
             {:mday => 15, :name => "Proclamação da República", :regions => [:br]}],
       12 => [{:mday => 25, :name => "Natal", :regions => [:br]}]
       }
--- lib/holidays/version.rb
+++ lib/holidays/version.rb
@@ -1,3 +1,3 @@
 module Holidays
-  VERSION = '8.4.4'
+  VERSION = '8.4.5'
 end
